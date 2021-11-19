ac11f0b8cc126061c3e7e76a37fe5d8a7e6165a2
Pavel <80854516+brudi16@users.noreply.github.com>
2021-11-19 16:39:16 +0100

    Added files

---
+++ Core/Inc/measuring.h
@@ -0,0 +1,52 @@
+/** ***************************************************************************
+ * @file
+ * @brief See measuring.c
+ *
+ * Prefixes MEAS, ADC, DAC
+ *
+ *****************************************************************************/
+
+#ifndef MEAS_H_
+#define MEAS_H_
+
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+#include <stdbool.h>
+
+
+/******************************************************************************
+ * Defines
+ *****************************************************************************/
+extern bool MEAS_data_ready;
+uint32_t MEAS_input_count;
+extern bool DAC_active;
+
+
+/******************************************************************************
+ * Functions
+ *****************************************************************************/
+void MEAS_GPIO_analog_init(void);
+void MEAS_timer_init(void);
+void DAC_reset(void);
+void DAC_init(void);
+void DAC_increment(void);
+void ADC_reset(void);
+void ADC3_IN4_single_init(void);
+void ADC3_IN4_single_read(void);
+void ADC3_IN4_timer_init(void);
+void ADC3_IN4_timer_start(void);
+void ADC3_IN4_DMA_init(void);
+void ADC3_IN4_DMA_start(void);
+void ADC1_IN13_ADC2_IN5_dual_init(void);
+void ADC1_IN13_ADC2_IN5_dual_start(void);
+void ADC2_IN13_IN5_scan_init(void);
+void ADC2_IN13_IN5_scan_start(void);
+void ADC3_IN13_IN4_scan_init(void);
+void ADC3_IN13_IN4_scan_start(void);
+
+void MEAS_show_data(void);
+
+
+#endif
