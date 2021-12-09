1aa125efdfa542a9bbc2ed7b3698ba0da6f2dfef
Yves Röhrig <79026537+yvesroehrig@users.noreply.github.com>
2021-12-09 13:29:15 +0100

    Delete StartGccBuildEnvironment.bat

--- StartGccBuildEnvironment.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem *************************************************************************************************
-rem Please do NOT change the following settings
-rem *************************************************************************************************
-set PlatformName=STM32F429-Discovery
-set BuildEnvVersion=V11.00
-set GettingStartedLink=getting-started-stm32f429-discovery
-cmd /K Application\Project\GCC\devenv.cmd
