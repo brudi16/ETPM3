f27917166ec9ac095ea2487911d696836a061e4e
Pavel <80854516+brudi16@users.noreply.github.com>
2021-12-09 12:48:16 +0100

    functions added and edited measuring and main

--- Core/Src/LUT.c
+++ Core/Src/LUT.c
@@ -13,6 +13,7 @@
  * Includes
  *****************************************************************************/
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "../Inc/LUT.h"
 
@@ -41,4 +42,37 @@ void initLUT(void){
             hallLut[i] = hallLutStartValues[i];
         }
     #endif
+}
+
+/**
+ * @brief interpolate Calibration values
+ * 
+ * @param x Array with calibration Distances
+ * @param y Array with measured calibration values
+ * @param xySize Size of the calibration array
+ */
+void interpolCalDistance(int16_t x[], int16_t y[], int32_t xySize){
+    int16_t i,j;
+    bool l;
+    
+    // loop for the different distances
+    for(i=0;i<xySize;i++){
+        l = false;
+        // loop for Values
+        for(j=0;j<x[i];j++){
+            // set start value of j to value from last loop
+            if(i>0 && l == false){
+                j = x[i-1];
+                l = true;
+            }
+            if(j == 0){
+                // do nothing
+            }else if(j< x[0]){
+                // for first intervall of distance
+                padLut[j] = LinearInterpol(1,padLutStartValues[0],x[0],y[0],(j+1));
+            }else{
+                padLut[j] = LinearInterpol(x[(i-1)],y[(i-1)],x[(i)],y[(i)],(j+1));
+            }
+        }
+    }
 }
--- testcode/array.c
+++ testcode/array.c
@@ -21,7 +21,7 @@ uint32_t calc_RMSValue (int32_t ADC_samples[], uint16_t size);
 float calc_distance (int32_t ADC_samples[], uint16_t size);
 float calc_current(int32_t ADC_samples[], uint16_t size);
 int32_t LinearInterpol(int32_t x0, int32_t x1, int32_t y0, int32_t y1, int32_t xp);
-int32_t interpolCalValues(int16_t x[], int16_t y[],int32_t xySize ,int32_t lut[]);
+void interpolCalValuesDistance(int16_t x[], int16_t y[], int16_t xySize);
 void init_LUT(void);
 
 // Global variables
@@ -34,24 +34,24 @@ const int32_t hallLutStartValues[LUT_SIZE] = {
 
 int32_t padLut[LUT_SIZE];
 
-int16_t measCalDistance[] = {2,       5,   10,  50, 100, 150, 200};
-int16_t testInterpol[]    = {4093, 2048, 1517, 946, 814, 753, 715};
+int16_t measCalDistance[] = {2,       5,   10,   20,  50, 100, 200};
+int16_t testInterpol[]    = {3972, 2048, 1517, 1204, 946, 814, 715};
 
 int main(){
-    int32_t sum, mean, peakPeak, rmsValue, interpol;
+    int32_t sum, mean, peakPeak, rmsValue, interpol, x;
 
     int32_t array[] = {1000,1259,1447,1511,1433,1236,973,718,540,491,582,789,1054,1305,1471,1506,1402,1186,919,674,519,498,615,840,1108,1347,1489,1496,1366,1134,866,634,504,511,653,892,1160,1385,1502,1481,1326,1081,814,598,494,529,695,946,1211,1418,1509,1460,1282,1027,764,567,489,553,741,1000};
-    printArray(array, ARRAYSIZE);
-    printf("Adress: %p\n", array);
+    // printArray(array, ARRAYSIZE);
+    // printf("Adress: %p\n", array);
 
-    mean = arrayMean(array, ARRAYSIZE);
-    printf("Mean: %d\n", mean);
-    printf("Adress: %p\n", array);
+    // mean = arrayMean(array, ARRAYSIZE);
+    // printf("Mean: %d\n", mean);
+    // printf("Adress: %p\n", array);
 
-    removeMean(array, ARRAYSIZE);
-    printf("\nMean removed:\n");
-    printArray(array, ARRAYSIZE);
-    printf("Adress: %p\n", array);
+    // removeMean(array, ARRAYSIZE);
+    // printf("\nMean removed:\n");
+    // printArray(array, ARRAYSIZE);
+    // printf("Adress: %p\n", array);
 
     //rectify(array, ARRAYSIZE);
     //printf("\nRectified:\n");
@@ -60,20 +60,24 @@ int main(){
 
     //forTestLoops(4,5);
 
-    peakPeak = calc_peakToPeak_av(array, (sizeof(array)/(sizeof(uint32_t))));
-    printf("\nPeak to Peak Value in average: %d\n",peakPeak);
+    // peakPeak = calc_peakToPeak_av(array, (sizeof(array)/(sizeof(uint32_t))));
+    // printf("\nPeak to Peak Value in average: %d\n",peakPeak);
 
-    rmsValue = calc_RMSValue(array, (sizeof(array)/(sizeof(uint32_t))));
-    printf("\nRMS-Value: %d\n", rmsValue);
+    // rmsValue = calc_RMSValue(array, (sizeof(array)/(sizeof(uint32_t))));
+    // printf("\nRMS-Value: %d\n", rmsValue);
 
     //printArray(padLutStartValues,LUT_SIZE);
     //init_LUT();
     //printArray(padLut, LUT_SIZE);
+    init_LUT();
 
     interpol = LinearInterpol(100, 814, 200, 715, 150);
-    printf("\nInterpolated Value for 20: %d\n", interpol);
+    printf("\nInterpolated Value for 150: %d\n", interpol);
     
-    interpolCalValues(measCalDistance,testInterpol, CAL_SIZE, padLut);
+    interpolCalValuesDistance(measCalDistance,testInterpol, CAL_SIZE);
+
+    x = getXFromY(padLut, LUT_SIZE, 1200);
+    printf("\nDistance: %d\n",x);
     return 0;
 }
 
@@ -213,26 +217,81 @@ int32_t LinearInterpol(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t x
 
     return (int32_t)yp;
 }
-
-int32_t interpolCalValues(int16_t x[], int16_t y[],int32_t xySize ,int32_t array[]){
-    int16_t i,j,k;
-    int32_t interpVal = 0;
+/**
+ * @brief interpolate Calibration values
+ * 
+ * @param x 
+ * @param y 
+ * @param xySize 
+ */
+void interpolCalValuesDistance(int16_t x[], int16_t y[], int16_t xySize){
+    int16_t i,j;
     bool l;
     
-
+    // loop for the different distances
     for(i=0;i<xySize;i++){
         l = false;
+        // loop for Values
         for(j=0;j<x[i];j++){
+            // set start value of j to value from last loop
             if(i>0 && l == false){
                 j = x[i-1];
                 l = true;
             }
-            if(j == x[i]){
-                array[j] = y[i];
+            if(j == 0){
+                // do nothing
+            }else if(j< x[0]){
+                // for first intervall of distance
+                padLut[j] = LinearInterpol(1,padLutStartValues[0],x[0],y[0],(j+1));
+            }else{
+                padLut[j] = LinearInterpol(x[(i-1)],y[(i-1)],x[(i)],y[(i)],(j+1));
             }
-            printf("i: %d\tj: %d\tx: %d\ty: %d\n",i,j,array[j],y[i]);
+            //printf("i: %d\tj: %d\tdefault: %d\tnew: %d\tx: %d\ty: %d\n",i,j,padLutStartValues[j],padLut[j],x[i],y[i]);
             
         }
     }
-    return 0;
+}
+/**
+ * @brief Search the corresponding x-value to a given y-value
+ * 
+ * @param array     Array with y-values
+ * @param size      Size of the array
+ * @param yValue    y-value of the searched x-value
+ * @return int32_t 
+ */
+int32_t getXFromY(int32_t array[], int32_t size, int32_t yValue){
+    int32_t xValue = 0; // Output variable
+    int16_t i;      // Loop variable
+
+    // Loop for all values of the array
+    for(i=0; i<(size - 1); i++){
+        printf("\ni: %d\t array[i]: %d\tarray[i+1]: %d",i, array[i], array[i+1]);
+        // Check if the value is lower than lowest or higher than highes value
+        if(yValue > array[0] || yValue < array[(size+1)]){
+            return 0;
+        
+        }else if(yValue == array[i]){
+            xValue = i+1;
+        // Check if y value is higher than next value and lower than actual value from array
+        }else if(yValue > array[(i+1)] && yValue < array[i]){
+            // Check if difference to next value from array is higher than difference to actual array element
+            if((yValue - array[(i+1)]) > (array[i] - yValue)){
+                xValue = i+1;
+            }else{
+                xValue = i+2;
+            }
+        }
+    }
+    
+    return xValue;
+}
+
+float calcStdDev(int32_t array[], int32_t size) {
+    float mean, SD = 0.0;
+    int i;
+    mean = calc_peakToPeak_av(array, size);
+    for (i = 0; i < size; ++i) {
+        SD += pow(array[i] - mean, 2);
+    }
+    return sqrt(SD / 10);
 }
