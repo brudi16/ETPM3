10c1091d3d54987525a5308985a2f393b857e1c6
Pavel <80854516+brudi16@users.noreply.github.com>
2021-12-28 18:07:35 +0100

    Update calculations.c
    - Comments added
    - Header improved

--- Application/Source/calculations.c
+++ Application/Source/calculations.c
@@ -2,10 +2,44 @@
  * @file
  * @brief Calculations of the measured values
  *
- * Functions for calculating values from measurements
+ * Functions for calculations
+ * ============================================================================
+ * 
+ * DC-Value
+ * ----------------------------------------------------------------------------
+ * - Calculate DC-value of the signal
+ * - Remove the DC-value of the signal
+ * 
+ * Calculations of Amplitude
+ * ----------------------------------------------------------------------------
+ * - Calculate peak to peak value of the signal
+ * - Calculate RMS-value of the signal
+ * 
+ * Interpolation
+ * ----------------------------------------------------------------------------
+ * - Linear interpolation
+ * 
+ * Array
+ * ----------------------------------------------------------------------------
+ * - Search function for an array
+ * - Copy function
+ * 
+ * Distance
+ * ----------------------------------------------------------------------------
+ * - Function for distance output
+ * 
+ * Statistic
+ * ----------------------------------------------------------------------------
+ * - Calculation of the standart deviation
+ * 
+ * Filter
+ * ----------------------------------------------------------------------------
+ * - FIR filtering function
+ * 
+ * 
  * @n
  *
- * @author  Pavel Müller, muellpav@students.zhaw.ch
+ * @authors Pavel Müller, Röhrig Yves
  * @date	28.11.2021
  *****************************************************************************/
 
@@ -27,29 +61,29 @@
  * Defines
  *****************************************************************************/
 // Activate for debugging of calculations
-//#define DEBUG																					///< Activate for debugging
+//#define DEBUG
 
 /******************************************************************************
  * Variables
  *****************************************************************************/
-///Arrays from measured Signal
-int32_t pad1Values[ADC_NUMS_ACU];												///< Copied values from pad 1
-int32_t pad2Values[ADC_NUMS_ACU];												///< Copied values from pad 2
-int32_t hall1Values[ADC_NUMS_ACU];											///< Copied values from Hall Sensor 1
-int32_t hall2Values[ADC_NUMS_ACU];											///< Copied values from Hall Sensor 2
+//Arrays from measured Signal
+int32_t pad1Values[ADC_NUMS_ACU];					    ///< Copied values from pad 1
+int32_t pad2Values[ADC_NUMS_ACU];					    ///< Copied values from pad 2
+int32_t hall1Values[ADC_NUMS_ACU];					    ///< Copied values from Hall Sensor 1
+int32_t hall2Values[ADC_NUMS_ACU];					    ///< Copied values from Hall Sensor 2
 
-uint32_t peakToPeakArrayPad1[NUM_PERIODS_MAX];												///< Pad 1 array with peak to peak values for every period of the signal
-uint32_t peakToPeakArrayPad2[NUM_PERIODS_MAX];												///< Pad 2 array with peak to peak values for every period of the signal
-uint32_t peakToPeakArrayHall1[NUM_PERIODS_MAX];											///< Hall Sensor 1 array with peak to peak values for every period of the signal
-uint32_t peakToPeakArrayHall2[NUM_PERIODS_MAX];											///< Hall Sensor 2 array with peak to peak values for every period of the signal
+uint32_t peakToPeakArrayPad1[NUM_PERIODS_MAX];		    ///< Pad 1 array with peak to peak values for every period of the signal
+uint32_t peakToPeakArrayPad2[NUM_PERIODS_MAX];		    ///< Pad 2 array with peak to peak values for every period of the signal
+uint32_t peakToPeakArrayHall1[NUM_PERIODS_MAX];		    ///< Hall Sensor 1 array with peak to peak values for every period of the signal
+uint32_t peakToPeakArrayHall2[NUM_PERIODS_MAX];		    ///< Hall Sensor 2 array with peak to peak values for every period of the signal
 
-int32_t debugArray[ADC_NUMS_ACU] = {										///< Array for debugging purpose
+int32_t debugArray[ADC_NUMS_ACU] = {				    ///< Array for debugging purpose
 	#include "debugArray.csv"
 };
 
-/// Arrays with filter coefficients
-const float aiir[] = {0.0023, 0, -0.0045, 0, 0.0023};   ///< A coefficients of the iir filter
-const float biir[] = {1.0, -3.4095, 4.7708, -3.1806};   ///< B coefficients of the iir filter
+// Arrays with filter coefficients
+const float32_t aiir[] = {0.0023, 0, -0.0045, 0, 0.0023};   ///< A coefficients of the iir filter
+const float32_t biir[] = {1.0, -3.4095, 4.7708, -3.1806};   ///< B coefficients of the iir filter
 
 const float32_t bfir[] = {                              ///< coefficients of the fir filter
     #include "bfir.csv"
@@ -62,12 +96,14 @@ const float32_t bfir[] = {                              ///< coefficients of the
 
 /** ***************************************************************************
  * @brief Calculate the DC-Value of the signal
+ *
+ * @param[in] ADC_samples   Array with measured values
+ * @param[in] size          Size of the Array
+ * @return int32_t      DC-Value of the signal
+ * 
  * This funciton calculates the DC-Value of the signal by calculating its mean.
  * The mean is calculted by summ up all values and divide by the number of values.
- *
- * @param ADC_samples   Array with measured values
- * @param size          Size of the Array
- * @return int32_t      DC-Value of the signal
+ * 
  *****************************************************************************/
 int32_t calc_dcValue(int32_t ADC_samples[], uint16_t size){
 	int32_t dcValue = 0;
@@ -82,10 +118,12 @@ int32_t calc_dcValue(int32_t ADC_samples[], uint16_t size){
 
 /** ***************************************************************************
  * @brief removes DC value form the array
- * Goes through every value of the Array and removes its DC-Value.
  * 
- * @param ADC_samples 	Array with measured values
- * @param size 			Size of the Array
+ * @param[in] ADC_samples 	Array with measured values
+ * @param[in] size 			Size of the Array
+ * 
+ * Goes through every value of the Array and removes its DC-Value. The number
+ * of array elements is set by the parameter size.
  *****************************************************************************/
 void calc_removeDc(int32_t ADC_samples[], uint16_t size){
 	int32_t dcValue;
@@ -102,12 +140,13 @@ void calc_removeDc(int32_t ADC_samples[], uint16_t size){
 /** ***************************************************************************
  * @brief calculate average peak to peak value
  * 
+ * @param[in] ADC_samples 	Array with measured values
+ * @param[in] size 			Size of the Array
+ * @param[in] peakPeakArray Array with peak to peaks values
+ * @return uint32_t 
+ * 
  * Calculate the peak to peak value for every period and average it for filtering.
  * 
- * @param ADC_samples 	Array with measured values
- * @param size 			Size of the Array
- * @param peakPeakArray Array with peak to peaks values
- * @return uint32_t 
  *****************************************************************************/
 uint32_t calc_peakToPeak_av(int32_t ADC_samples[], uint16_t size, uint32_t peakPeakArray[]){
 	uint16_t i1, i2;
@@ -158,7 +197,7 @@ uint32_t calc_RMSValue (int32_t ADC_samples[], uint16_t size){
     uint32_t rmsValue = 0;
     uint16_t i;
     int32_t tmp;
-    float tmpfloat = 0;
+    float32_t tmpfloat = 0;
 
 	// Sum up all squared Values from the Array
     for(i=0;i<size; i++){
@@ -174,6 +213,7 @@ uint32_t calc_RMSValue (int32_t ADC_samples[], uint16_t size){
     rmsValue = (uint32_t)tmpfloat;
     return rmsValue;
 }
+
 /** ***************************************************************************
  * @brief Linear interpolation between 2 given points
  * 
@@ -188,9 +228,9 @@ uint32_t calc_RMSValue (int32_t ADC_samples[], uint16_t size){
  * @return int32_t yp Y-Coordinate of the searched point 
  *****************************************************************************/
 int32_t LinearInterpol(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t xp){
-    float yp = 0;
+    float32_t yp = 0;
 	// Linear Interpolation between 2 points
-    yp = ((float)y0 - (((float)y0-(float)y1)/((float)x1-(float)x0)) * ((float)xp - (float)x0));
+    yp = ((float32_t)y0 - (((float32_t)y0-(float32_t)y1)/((float32_t)x1-(float32_t)x0)) * ((float32_t)xp - (float32_t)x0));
 
     return (int32_t)yp;
 }
@@ -204,8 +244,8 @@ int32_t LinearInterpol(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t x
  * @return int32_t 
  *****************************************************************************/
 uint32_t getXFromY(uint16_t array[], int32_t size, uint32_t yValue){
-    uint32_t xValue = 0; // Output variable
-    int16_t i;      // Loop variable
+    uint32_t xValue = 0;    // Output variable
+    int16_t i;              // Loop variable
 
     // Loop for all values of the array
     for(i=0; i<(size - 1); i++){
@@ -230,6 +270,7 @@ uint32_t getXFromY(uint16_t array[], int32_t size, uint32_t yValue){
     }  
     return xValue;
 }
+
 /** ***************************************************************************
  * @brief Calculate the standart deviation
  * 
@@ -239,11 +280,11 @@ uint32_t getXFromY(uint16_t array[], int32_t size, uint32_t yValue){
  * @param peakPeakArray1 Array with peak to peak values
  * @param peakPeakArray2
  * @param size  Size of the array
- * @return float 
+ * @return float32_t 
  *****************************************************************************/
 uint32_t calcStdDev(uint32_t peakPeakArray1[], uint32_t peakPeakArray2[], int32_t size) {
-    float mean1 = 0, mean2 = 0;
-    float SD = 0.0, SD1 = 0.0, SD2 = 0.0;
+    float32_t mean1 = 0, mean2 = 0;
+    float32_t SD = 0.0, SD1 = 0.0, SD2 = 0.0;
     uint16_t i;
     uint32_t distanceArray1[NUM_PERIODS_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     uint32_t distanceArray2[NUM_PERIODS_MAX] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
